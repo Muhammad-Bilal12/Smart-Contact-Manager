7611f2cb36e115feb0801b2a71c3d8585f36da15
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2023-12-07 14:50:21 +0500

    setting bootstrap templates

--- smart_contact_manager/src/main/java/com/smart/smart_contact_manager/controller/UserController.java
+++ smart_contact_manager/src/main/java/com/smart/smart_contact_manager/controller/UserController.java
@@ -2,6 +2,7 @@ package com.smart.smart_contact_manager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,24 +14,16 @@ import com.smart.smart_contact_manager.entity.User;
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
+    }
 
-    @GetMapping("/test")
-    @ResponseBody
-    public String test() {
-
-        User user = new User();
-        user.setName("Bilal");
-        user.setEmail("bilal@gmail.com");
-
-        Contact contact = new Contact();
-        contact.setName("Noman");
-        user.getContacts().add(contact);
-
-        userRepository.save(user);
-
-        return "working...";
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
     }
 
 }
