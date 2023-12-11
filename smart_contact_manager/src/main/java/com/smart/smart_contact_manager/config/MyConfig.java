79c800026fe0b9ad94ea8c36d0ce8af605367ef9
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2023-12-11 15:19:00 +0500

    Error to solve for Authentaction Manager

--- smart_contact_manager/src/main/java/com/smart/smart_contact_manager/config/MyConfig.java
+++ smart_contact_manager/src/main/java/com/smart/smart_contact_manager/config/MyConfig.java
@@ -2,40 +2,36 @@ package com.smart.smart_contact_manager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import jakarta.servlet.Filter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class MyConfig extends WebSecurityConfiguration {
+public class MyConfig {
 
-    @Bean
-    UserDetailsService getUserDetailsService() {
-        return new UserDetailsServicesImpl();
-    }
+    // @Bean
+    // UserDetailsService getUserDetailsService() {
+    // return new UserDetailsServicesImpl();
+    // }
 
-    @Bean
-    BCryptPasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // BCryptPasswordEncoder getPasswordEncoder() {
+    // return new BCryptPasswordEncoder();
+    // }
 
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+    // public DaoAuthenticationProvider authenticationProvider() {
+    // DaoAuthenticationProvider daoAuthenticationProvider = new
+    // DaoAuthenticationProvider();
 
-        daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
-        daoAuthenticationProvider.setPasswordEncoder(this.getPasswordEncoder());
+    // daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
+    // daoAuthenticationProvider.setPasswordEncoder(this.getPasswordEncoder());
 
-        return daoAuthenticationProvider;
+    // return daoAuthenticationProvider;
 
-    }
+    // }
 
-    // Configure Method
+    // Newer version
 
 }
